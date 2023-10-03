ab977d1429c93554fa81d6dee356b2a6a5e4fb1c
Yvonne Okolo <yokolo@hotmail.co.uk>
2023-10-03 21:46:00 +0100

    Start on JSON exercises

---
+++ src/main/java/com/cbfacademy/FileHandler/JSONFileHandler.java
@@ -0,0 +1,14 @@
+package main.java.com.cbfacademy.FileHandler;
+
+import com.cbfacademy.Employee.Employee;
+
+public class JSONFileHandler {
+    
+    public List<Employee> readFile(String filename) {
+        
+    }
+
+    public void save(Employee employee, String outputFile) {
+
+    }
+}
